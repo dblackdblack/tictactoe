9bb1d2e11aa21035b49e4f99b9630f22e87e4b4e
David Black <dblack@bepress.com>
2014-11-03 19:26:41 -0800

    fix circular import

--- forms.py
+++ forms.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from sqlalchemy.orm.exc import NoResultFound
 
-from tictactoe import User
+import tictactoe
 from flask_wtf import Form
 from wtforms import StringField
 from wtforms.validators import DataRequired, ValidationError, Length
@@ -17,7 +17,7 @@ class LoginForm(Form):
 
     def validate_username(self, field):
         try:
-            User.query.filter_by(username=field.data).one()
+            tictactoe.User.query.filter_by(username=field.data).one()
         except NoResultFound:
             raise ValidationError("invalid username '%s'" % field.data)
 
@@ -28,5 +28,6 @@ class NewUserForm(Form):
                                 Length(max=MAX_USERNAME_LENGTH)))
 
     def validate_username(self, field):
-        if User.query.filter_by(username=field.data).limit(1).scalar():
+        if tictactoe.User.query.filter_by(
+                username=field.data).limit(1).scalar():
             raise ValidationError("username '%s' already exists" % field.data)
